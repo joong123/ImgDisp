efd33e05a886ed3c4fb6638d4b3e664b329fe9b6
joong <1512735634@qq.com>
2020-12-06 15:40:31 +0800

    Change output directory & int directory(中间目录) of Projects; Change default code in Dlg;

--- ImgDisp/ImgDispDlg.cpp
+++ ImgDisp/ImgDispDlg.cpp
@@ -99,9 +99,9 @@ BOOL CImgDispDlg::OnInitDialog()
 	SetIcon(m_hIcon, TRUE);			// 设置大图标
 	SetIcon(m_hIcon, FALSE);		// 设置小图标
 
-	ShowWindow(SW_MAXIMIZE);
+	//ShowWindow(SW_MAXIMIZE);
 
-	ShowWindow(SW_MINIMIZE);
+	//ShowWindow(SW_MINIMIZE);
 
 	// TODO: 在此添加额外的初始化代码
 
